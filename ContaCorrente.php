0eea8a01c1261f16a32596d85d610cea1b21db92
raffharah <japa.rafael.hara@gmail.com>
2024-08-01 23:45:53 -0300

    Função de saque e deposito criados.

--- ContaCorrente.php
+++ ContaCorrente.php
@@ -5,46 +5,77 @@ class ContaCorrente
     public function __construct(private int $numeroConta, private string $titular, private string $cpf, private string $email, private string $celular, private float $saldo)
     {
     }
+
     //Getters
     public function getConta(): int
     {
         return $this->numeroConta;
     }
+
     public function getTitular(): string
     {
         return $this->titular;
     }
+
     public function getCpf(): string
     {
         return $this->cpf;
     }
+
     public function getEmail(): string
     {
         return $this->email;
     }
+
     public function getCelular(): string
     {
         return $this->celular;
     }
+
     //Setters
     public function setTitular(string $titular): void
     {
         $this->titular = $titular;
     }
+
     public function getSaldo(): float
     {
         return $this->saldo;
     }
+
     public function setSaldo(float $saldo): void
     {
         $this->saldo = $saldo;
     }
+
     public function setEmail(string $email): void
     {
         $this->email = $email;
     }
+
     public function setCelular(string $celular): void
     {
         $this->celular = $celular;
     }
+    public function deposita(float $deposito)
+    {
+        if ($deposito >= 1) {
+            $this->saldo += $deposito;
+            echo "Seu deposito de $$deposito foi efetuado com sucesso";
+        } else {
+            echo "Valor de depósito inválido.";
+        }
+    }
+    public function saque(float $saque)
+    {
+        if ($saque > $this->saldo) {
+            echo "Valor de saque inválido, saldo indisponivel.";
+        } elseif ($saque <= 0) {
+            echo "Valor de saque inválido.";
+        } else{
+        $this->saldo -= $saque;
+        echo "Seu saque de $$saque foi efetuado com sucesso";
+    }
+}
+
 }
