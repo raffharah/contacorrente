f5416dce0255c1319caa18c66795aa389bd76114
raffharah <japa.rafael.hara@gmail.com>
2024-08-01 22:56:47 -0300

    Nada de novo, só colocando mais contas-correntes para criar posteriormente funções, como depositar e saque.

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
 require __DIR__ . "/ContaCorrente.php";
 
+$contaCorrente1 = new ContaCorrente(87865, 'J. J. Jameson', "12345678910", "j.j.jameson@bol.com", "(11)91234-5679", 10000);
+$contaCorrente2 = new ContaCorrente(21313, 'Tio Ben', "12345678911", "tio.ben@uol.com", "(11)91234-5678", 1000);
+$contaCorrente3 = new ContaCorrente(21313, 'Tia May', "12345678912", "tia.may@uol.com", "(11)91234-5672", 1000);
+$contaCorrente4 = new ContaCorrente(21313, 'Mary Jane', "12345678932", "mary.jane@google.com", "(11)91434-5672", 400);
+$contaCorrente4 = new ContaCorrente(21313, 'Miranha', "12345678933", "eu.nao.sou.miranha@outlook.com", "(11)91232-5672", 100);
+
+echo $contaCorrente4->getSaldo();
